b535730524fec6e95261993edd97daacd23be763
Bharat Panwar <55885130+bsingh23@users.noreply.github.com>
2021-03-08 16:24:55 +0530

    updated ":" in print("   group:")

--- nor_inv.py
+++ nor_inv.py
@@ -1,13 +1,13 @@
 def nor_inv(filename): 
     with open(filename) as f: 
         data = f.read().splitlines()
-        #List Comprehension create a new list which contains tuple of hostname and ip-address  
+        #Create a new list which contains tuple of hostname and ip-address  
         newlist = [tuple(value.split(" ")) for value in data]
         #Tuple unpacking for creating nornir inventory
         for name,ip in newlist: 
             print(f"{name.upper()}:") 
             print(f"   hostname: {ip}") 
-            print("   group") 
+            print("   group:") 
             site_abb = name.split("-") 
             print(f"       - {site_abb[0].upper()}")
             print("   platform: 'ios'")
